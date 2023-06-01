72bc76670f29388737287ced79935b8e32fed43a
BUDZ1974 <92312281+budz123@users.noreply.github.com>
2023-06-01 20:41:07 +0300

    DIplom

---
+++ Новая папка (5)/diplomliza1/ViewModels/AcceptViewModel.cs
@@ -0,0 +1,38 @@
+﻿using diplomliza1.Data;
+using diplomliza1.Data.Entities;
+using diplomliza1.Services;
+using diplomliza1.Storage.Enums;
+using diplomliza1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace diplomliza1.ViewModels
+{
+    public class AcceptViewModel : ViewModelBase
+    {
+        public AcceptViewModel(ApplicationDbContext ctx ,Questionnare questionnare)
+        {
+            _questionareService = new(ctx);
+            _questionnare = questionnare;
+            DateOfMeeting = DateTime.Now;
+        }
+        private QuestionareService _questionareService;
+        private Questionnare _questionnare;
+        private DateTime _dateOfMeeting;
+        public DateTime DateOfMeeting { get => _dateOfMeeting; set => Set(ref _dateOfMeeting, value, nameof(DateOfMeeting)); }
+        public Questionnare? Questionnare { get => _questionnare; set => Set(ref _questionnare, value, nameof(Questionnare)); }
+        private void EditQuestionnare()
+        {
+             Questionnare.Status = Statuse.Принято.ToString();
+             Questionnare.MeetingDate = DateOfMeeting;
+              _questionareService.Update(Questionnare);
+              MessageBox.Show($"Вопрос обновлён!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public ICommand EditQuestionnareButton => new Command(addreview => EditQuestionnare());
+    }
+}
---
+++ Новая папка (5)/diplomliza1/ViewModels/ApplicantVacancyViewModePage.cs
@@ -0,0 +1,61 @@
+﻿using diplomliza1.Controls;
+using diplomliza1.Data;
+using diplomliza1.Data.Entities;
+using diplomliza1.Services;
+using diplomliza1.View;
+using diplomliza1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace diplomliza1.ViewModels
+{
+    public class ApplicantVacancyViewModePage : ViewModelBase
+    {
+
+        public ApplicantVacancyViewModePage(ApplicationDbContext ctx,JobVacancyService jobVacancyService, User applicant,QuestionareService questionareService)
+        {
+            _jobVacancyService = jobVacancyService;
+            _questionareService = questionareService;
+            _ctx = ctx;
+            _applicant = applicant;
+            UpdateLists();
+        }
+        private ApplicationDbContext _ctx;
+        private QuestionareService _questionareService;
+        private JobVacancyService _jobVacancyService;
+        private User _applicant;
+        private List<JobVacancy> _jobTitles;
+        private string _searchJobTitle;
+        private JobVacancy _selectedJobTitle;
+        private Photo _selectedphoto;
+        public JobVacancy SelectedJobTitle { get => _selectedJobTitle; set => Set(ref _selectedJobTitle, value, nameof(SelectedJobTitle)); }
+        public List<JobVacancy> JobTitles { get => _jobTitles; set => Set(ref _jobTitles, value, nameof(JobTitles)); }
+        private ICollection<JobVacancy> GetJobVacancy() => _jobVacancyService.GetJobTitle().ToList();
+        private bool SelectedJobTitleIsNull() => SelectedJobTitle == null;
+        private void UpdateLists()
+        {
+            JobTitles = new List<JobVacancy>(GetJobVacancy());
+
+        }
+        private void OpenAddJobTitleManagerWindow()
+        {
+            
+            if (!SelectedJobTitleIsNull())
+            {
+
+                new QuestionnaireWindow(_ctx, SelectedJobTitle, _jobVacancyService,_questionareService, _applicant).ShowDialog();
+                UpdateLists();
+            }
+            else
+                MessageBox.Show("Выберите услуг", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Information);
+          
+        }
+        public ICommand OpenAddManagerWindow => new Command(addquestion => OpenAddJobTitleManagerWindow());
+    }
+
+}
